7f870b1fc50c7ed49ae964b94a60d315bc8a3729
guymaily06 <guymaily06@gmail.com>
2025-01-25 00:14:12 -0600

    fixing line 31

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def sort_on(d):
 def chars_dict_to_sorte_list(num_chars_dict):
     sorted_list = []
     for ch in num_chars_dict:
-        sorted_list.append ({"char": ch, "num": num_chars_dict [ch]})
+        sorted_list.append ({"char": ch, "num": num_chars_dict[ch]})
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
     

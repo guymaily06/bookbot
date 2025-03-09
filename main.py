d30d30618fcaf2f84d64b5788860386b208a6fd0
guymaily06 <guymaily06@gmail.com>
2025-03-09 16:30:40 +0300

    yup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    book_path = "../books/frankenstein.txt"
+    book_path = "/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
